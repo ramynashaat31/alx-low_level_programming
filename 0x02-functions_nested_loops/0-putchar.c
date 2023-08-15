82563c10726663131259cf59fea42daa2358b9a2
Ramy Nashaat <139711853+ramynashaat31@users.noreply.github.com>
2023-08-16 00:45:50 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *main - print _putchar
- *is written by mbah
- *Return: 0
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
 int main(void)
@@ -15,6 +15,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('/n');
+_putchar('\n');
 return (0);
-} 
+}
