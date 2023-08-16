3c7e00c9c81f20934fe96d2fc12b70eba86138fe
ramynashaat31 <ramynashaat31@gmail.com>
2023-08-16 14:05:13 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,52 +1,24 @@
 #include <stdio.h>
+
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
 int main(void)
 {
-int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
-}
+	int i;
 
+	printf("Infinite loop incoming :(\n");
 
+	i = 0;
 
-2-largest_number.c
+	/* while (i < 10)*/
+	/* {*/
+	/*	putchar(i);*/
+	/* }*/
 
-#include "main.h"
-/**
-* largest_number - returns the largest of 3 numbers
-* @a: first integer
-* @b: second integer
-* @c: third integer
-* Return: largest number
-*/
-int largest_number(int a, int b, int c)
-{
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
-}
+	printf("Infinite loop avoided! \\o/\n");
 
+	return (0);
+}
