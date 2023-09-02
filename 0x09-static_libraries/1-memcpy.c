c83c6abf394ed9fae96da055b1cb7f117c0a2e29
Ramy Nashaat <139711853+ramynashaat31@users.noreply.github.com>
2023-09-02 10:29:31 +0300

    1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -5,12 +5,13 @@
  *@src: memory where is copied
  *@n: number of bytes
  *
- *Return: copied memory with n bytes changed
+ *Return: copied memory with n byted changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
+
 
 	for (; r < i; r++)
 	{
